8db118ed469e1e544bf50743940e541510eccac8
Rachel Aguinaldo <rachelnaguinaldo@gmail.com>
2023-11-25 22:01:17 -0500

    created Department class

---
+++ cop3703-projectpart2/src/Department.java
@@ -0,0 +1,18 @@
+
+public class Department {
+
+	public String name;
+	public String code;
+	public int officeNumber;
+	public String officePhone;
+	public Doctor deptHead;
+	
+	public Department(String name, String code, int officeNumber, String officePhone, Doctor deptHead) {
+		this.name = name;
+		this.code = code;
+		this.officeNumber = officeNumber;
+		this.officePhone = officePhone;
+		this.deptHead = deptHead;
+	}
+
+}
